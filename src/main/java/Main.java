bc11a3151c7dfb2ecffaf0616231235e098b8e4f
Maloulou99 <baha0610@stud.kea.dk>
2022-11-14 09:33:24 +0100

    Added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.makeSound();
+        cat.makeSound();
+
+
+
+    }
+}
